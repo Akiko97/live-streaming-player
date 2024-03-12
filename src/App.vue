945415881c7616e8982b15bc1e8aff4d0074041b
Gabriel Zhong <gabzhong@gmail.com>
2024-03-12 22:43:14 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <el-container>
+    <el-header>
+      <div class="header-container">
+        <el-text type="primary" size="large" tag="b">Live Streaming</el-text>
+        <el-text type="warning" size="small" tag="sup">Secret</el-text>
+      </div>
+    </el-header>
+    <el-main>
+      <HelloWorld msg="Vite + Vue" />
+    </el-main>
+  </el-container>
+</template>
+
+<style scoped>
+.header-container {
+  height: 100%;
+  box-shadow: var(--el-box-shadow-light);
+}
+</style>
